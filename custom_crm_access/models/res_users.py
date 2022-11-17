d9332a0a7512774cb833bab99a6c49216f714582
Erick Abrego <programador2@grupoley.com>
2022-11-17 12:54:35 -0700

    Update res_users.py

--- custom_crm_access/models/res_users.py
+++ custom_crm_access/models/res_users.py
@@ -5,3 +5,9 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     x_crm_team_ids = fields.Many2many(comodel_name="crm.team", string="Equipos de venta")
+
+    def write(self,vals):        
+        res = super(ResUsers,self).write(vals)
+        if vals.get('x_crm_team_ids'):
+            self.env['ir.rule'].clear_cache()
+        return res
