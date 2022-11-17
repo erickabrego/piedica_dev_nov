e57797b208e2c273f7945346c9f1d569a470248e
Erick Abrego <programador2@grupoley.com>
2022-11-17 11:42:08 -0700

    Update crm_access_rule.py

--- custom_crm_access/models/crm_access_rule.py
+++ custom_crm_access/models/crm_access_rule.py
@@ -9,7 +9,14 @@ class CRMAccessRule(models.Model):
     company_id = fields.Many2one(comodel_name="res.company", string="Compañía")
     company_ids = fields.Many2many(comodel_name="res.company", string="Compañías permitidas")
 
+    def write(self,vals):
+        res = super(CRMAccessRule,self).write(vals)
+        self.env['ir.rule'].clear_cache()
+        return res
+
     @api.depends("company_id")
     def _get_name_rule(self):
         for rec in self:
             rec.name = f"Acceso a CRM para {rec.company_id.name}"
+    
+
